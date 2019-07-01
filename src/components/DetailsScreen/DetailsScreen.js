3ecb064edccd4980e32490cddb46ea1cebbcaf00
wscats <2587575267@qq.com>
2019-07-01 12:45:58 +0800

    更新详情页

--- src/components/DetailsScreen/DetailsScreen.js
+++ src/components/DetailsScreen/DetailsScreen.js
@@ -26,11 +26,11 @@ export default class DetailsScreen extends Component {
     render() {
         return (
 
-<View style={{ flex: 1 }}>
-    <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
-    <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
-    <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
-</View>
+            <View style={{ flex: 1 }}>
+                <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
+                <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+                <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
+            </View>
         );
     }
 }
